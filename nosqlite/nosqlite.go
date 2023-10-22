d5ee6ac7d29734b947a27f700c5d8e686c022597
Jacek Kubicki <jacnik@gmail.com>
2023-10-22 11:44:15 +0200

    added typed structures to hold intermediate json data

--- nosqlite/nosqlite.go
+++ nosqlite/nosqlite.go
@@ -11,6 +11,49 @@ import (
 	"strconv"
 )
 
+type separator byte
+
+const (
+	NUL separator = '\x00'
+	SOH separator = '\x01'
+	STX separator = '\x02'
+	ETX separator = '\x03'
+	LF  separator = '\x0A'
+	FF  separator = '\x0C'
+	DEL separator = '\x7f'
+)
+
+type indexType byte
+
+const (
+	floatType indexType = 'f'
+	strType   indexType = 's'
+	nullType  indexType = 'n'
+)
+
+type valueRefs struct {
+	value jsonTypeValue
+	refs  []int
+}
+
+func (v valueRefs) String() string {
+	return fmt.Sprintf("%v %v", v.value, v.refs)
+}
+
+type propRefs struct {
+	prop   string
+	values []valueRefs
+}
+
+type jsonTypeValue struct {
+	_type indexType
+	value interface{}
+}
+
+func (p jsonTypeValue) String() string {
+	return fmt.Sprintf("%v{%c}", p.value, p._type)
+}
+
 func check(err error) {
 	if err != nil {
 		log.Fatal(err)
@@ -48,40 +91,44 @@ func parseJson(bytes []byte) interface{} {
 	return result
 }
 
-func flattenJsonMap(acc map[string]interface{}, prefix string, jMap map[string]interface{}) {
+func flattenJsonMap(acc map[string]jsonTypeValue, prefix string, jMap map[string]interface{}) {
 	for key, jItem := range jMap {
 		flattenWithPrefix(acc, prefix+"/"+key, jItem)
 	}
 }
 
-func flattenJsonArr(acc map[string]interface{}, prefix string, jArr []interface{}) {
+func flattenJsonArr(acc map[string]jsonTypeValue, prefix string, jArr []interface{}) {
 	for i, jItem := range jArr {
 		flattenWithPrefix(acc, prefix+"/"+strconv.Itoa(i), jItem)
 	}
 }
 
-func flattenWithPrefix(acc map[string]interface{}, prefix string, unflatten interface{}) {
+func flattenWithPrefix(acc map[string]jsonTypeValue, prefix string, unflatten interface{}) {
 	switch v := unflatten.(type) {
 	case map[string]interface{}:
 		flattenJsonMap(acc, prefix, v)
 	case []interface{}:
 		flattenJsonArr(acc, prefix, v)
+	case string:
+		acc[prefix] = jsonTypeValue{strType, v}
+	case float64:
+		acc[prefix] = jsonTypeValue{floatType, v}
 	default:
-		acc[prefix] = v
+		acc[prefix] = jsonTypeValue{nullType, v}
 	}
 }
 
-func flattenJson(unflatten interface{}) map[string]interface{} {
-	flatten := make(map[string]interface{})
+func flattenJson(unflatten interface{}) map[string]jsonTypeValue {
+	flatten := make(map[string]jsonTypeValue)
 	flattenWithPrefix(flatten, "", unflatten)
 	return flatten
 }
 
-func aggregateJson(agg map[string]map[interface{}][]int, flatten map[string]interface{}, fileIdx int) {
+func aggregateJson(agg map[string]map[jsonTypeValue][]int, flatten map[string]jsonTypeValue, fileIdx int) {
 	for prop, value := range flatten {
 		propMap, hasPropMap := agg[prop]
 		if !hasPropMap {
-			propMap = make(map[interface{}][]int)
+			propMap = make(map[jsonTypeValue][]int)
 			propMap[value] = []int{fileIdx}
 			agg[prop] = propMap
 		} else {
@@ -90,51 +137,29 @@ func aggregateJson(agg map[string]map[interface{}][]int, flatten map[string]inte
 	}
 }
 
-type valueRefs struct {
-	value interface{}
-	refs  []int
-}
-
-type propRefs struct {
-	prop   string
-	values []valueRefs
-}
-
-func sortValues(values map[interface{}][]int) []valueRefs {
-	lessStr := func(a string, b interface{}) int {
-		switch v := b.(type) {
-		case string:
-			return cmp.Compare(a, v)
-		case float64:
-			return 1 // floats compared to strings will be treated as smaller
-		}
-		return 1 // b is most likely nil
-	}
-
-	lessFloat := func(a float64, b interface{}) int {
-		switch v := b.(type) {
-		case float64:
-			return cmp.Compare(a, v)
-		case string:
-			return -1 // floats compared to strings will be treated as smaller
-		}
-		return 1 // b is most likely nil
-	}
-
-	less := func(a, b interface{}) int {
+func sortValues(values map[jsonTypeValue][]int) []valueRefs {
+	less := func(a, b jsonTypeValue) int {
 		/* Should return a negative number when a < b,
 		** a positive number when a > b
 		** and zero when a == b. */
-		switch v := a.(type) {
-		case string:
-			return lessStr(v, b)
-		case float64:
-			return lessFloat(v, b)
+		typesCmp := cmp.Compare(a._type, b._type)
+		if typesCmp != 0 {
+			return typesCmp
 		}
-		return -1 // a is most likely nil
+
+		switch a._type {
+		case floatType:
+			return cmp.Compare(a.value.(float64), b.value.(float64))
+		case strType:
+			return cmp.Compare(a.value.(string), b.value.(string))
+		case nullType:
+			return 0
+		}
+		message := fmt.Sprintf("Unknown type %c", a._type)
+		panic(message)
 	}
 
-	keys := make([]interface{}, 0, len(values))
+	keys := make([]jsonTypeValue, 0, len(values))
 	for k := range values {
 		keys = append(keys, k)
 	}
@@ -148,10 +173,40 @@ func sortValues(values map[interface{}][]int) []valueRefs {
 	return sortedRefs
 }
 
+func sortedKeys[K cmp.Ordered, V any](mapToSort map[K]V) []K {
+	keys := make([]K, 0, len(mapToSort))
+	for p := range mapToSort {
+		keys = append(keys, p)
+	}
+	slices.SortFunc(keys, cmp.Compare)
+	return keys
+}
+
+func createIndex(referenceAgg map[string]map[jsonTypeValue][]int) []propRefs {
+	index := make([]propRefs, 0, len(referenceAgg))
+	for _, p := range sortedKeys(referenceAgg) {
+		values := sortValues(referenceAgg[p])
+		index = append(index, propRefs{p, values})
+	}
+	return index
+}
+
+func printIndex(index []propRefs) {
+	// fmt.Println(index)
+
+	for _, ref := range index {
+		fmt.Printf("%v:", ref.prop)
+		for _, val := range ref.values {
+			fmt.Print(val, " ")
+		}
+	}
+	fmt.Printf("\n")
+}
+
 func main() {
 	dirPath := "./db"
 
-	agg := make(map[string]map[interface{}][]int)
+	agg := make(map[string]map[jsonTypeValue][]int)
 	for fileIdx, fileName := range listDir(dirPath) {
 		fmt.Println("Adding file:", fileName)
 
@@ -160,18 +215,6 @@ func main() {
 		aggregateJson(agg, flatten, fileIdx)
 	}
 
-	// todo sort props fn
-	properties := make([]string, 0, len(agg))
-	for p := range agg {
-		properties = append(properties, p)
-	}
-	slices.SortFunc(properties, cmp.Compare)
-
-	index := make([]propRefs, 0, len(properties))
-	for _, p := range properties {
-		values := sortValues(agg[p])
-		index = append(index, propRefs{p, values})
-	}
-
-	fmt.Println(index)
+	index := createIndex(agg)
+	printIndex(index)
 }
