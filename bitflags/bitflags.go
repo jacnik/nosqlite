6a3323ccc85b657dd0c7afb25de6ffadd49fa3fb
Jacek Kubicki <jacnik@gmail.com>
2023-10-29 16:58:52 +0100

    made bitflag functions into methods

--- bitflags/bitflags.go
+++ bitflags/bitflags.go
@@ -17,21 +17,19 @@ type BitField uint64
 const BITFIELD_EMPTY = BitField(0)
 const BITFIELD_FULL = BitField(0xFFFF_FFFF_FFFF_FFFF)
 
-func Set(b BitField, pos int) BitField    { return 1<<pos | b }
-func Clear(b BitField, pos int) BitField  { return ^(1 << pos) & b }
-func Toggle(b BitField, pos int) BitField { return 1<<pos ^ b }
-func Has(b BitField, pos int) bool        { return 1<<pos&b != 0 }
+func (b BitField) Set(pos int) BitField    { return 1<<pos | b }
+func (b BitField) Clear(pos int) BitField  { return ^(1 << pos) & b }
+func (b BitField) Toggle(pos int) BitField { return 1<<pos ^ b }
+func (b BitField) Has(pos int) bool        { return 1<<pos&b != 0 }
 
 // func Set(b, flag BitField) BitField    { return b | flag }
 // func Clear(b, flag BitField) BitField  { return b &^ flag }
 // func Toggle(b, flag BitField) BitField { return b ^ flag }
 // func Has(b, flag BitField) bool        { return b&flag != 0 }
 
-// func main() {
-// 	var b BitField
-// 	b = Set(b, F0)
-// 	b = Toggle(b, F2)
-// 	for i, flag := range []BitField{F0, F1, F2} {
-// 		fmt.Println(i, Has(b, flag))
-// 	}
+// type BitField interface {
+// 	Set(pos int) BitField
+// 	Clear(pos int) BitField
+// 	Toggle(pos int) BitField
+// 	Has(pos int) bool
 // }
--- bitflags/bitflags_test.go
+++ bitflags/bitflags_test.go
@@ -8,20 +8,19 @@ import (
 func TestCorrectlySetsBit(t *testing.T) {
 	b := BITFIELD_EMPTY
 
-	b = Set(b, 2)
+	b = b.Set(2)
 	if b != 0b100 {
 		t.Fatalf("Expected bit 2 to be set, instead got %04b.\n", b)
 	}
-
-	b = Set(b, 0)
+	b = b.Set(0)
 	if b != 0b101 {
 		t.Fatalf("Expected bit 0 to be set, instead got %04b.\n", b)
 	}
-	b = Set(b, 3)
+	b = b.Set(3)
 	if b != 0b1101 {
 		t.Fatalf("Expected bit 4 to be set, instead got %04b.\n", b)
 	}
-	b = Set(b, BITFIELD_SIZE)
+	b = b.Set(BITFIELD_SIZE)
 	if b != 0b1101 {
 		t.Fatalf("Expected bits beyond bitfield size nevet to be set.\n")
 	}
@@ -31,20 +30,19 @@ func TestCorrectlySetsBit(t *testing.T) {
 func TestCorrectlyClearsBit(t *testing.T) {
 	b := BitField(0b1101)
 
-	b = Clear(b, 2)
+	b = b.Clear(2)
 	if b != 0b1001 {
 		t.Fatalf("Expected bit 2 to be clear, instead got %04b.\n", b)
 	}
-
-	b = Clear(b, 0)
+	b = b.Clear(0)
 	if b != 0b1000 {
 		t.Fatalf("Expected bit 0 to be clear, instead got %04b.\n", b)
 	}
-	b = Clear(b, 3)
+	b = b.Clear(3)
 	if b != 0b0000 {
 		t.Fatalf("Expected bit 3 to be clear, instead got %04b.\n", b)
 	}
-	b = Clear(b, 0)
+	b = b.Clear(0)
 	if b != 0b0000 {
 		t.Fatalf("Expected bit 0 to be clear, instead got %04b.\n", b)
 	}
@@ -55,29 +53,29 @@ func TestCorrectlyToggledBit(t *testing.T) {
 	b := BitField(0b1101)
 
 	// v := b.Toggle(2)
-	b = Toggle(b, 2)
+	b = b.Toggle(2)
 	if b != 0b1001 {
 		t.Fatalf("Expected bit 2 to be clear, instead got %04b.\n", b)
 	}
-	b = Toggle(b, 2)
+	b = b.Toggle(2)
 	if b != 0b1101 {
 		t.Fatalf("Expected bit 2 to be set, instead got %04b.\n", b)
 	}
 
-	b = Toggle(b, 0)
+	b = b.Toggle(0)
 	if b != 0b1100 {
 		t.Fatalf("Expected bit 0 to be clear, instead got %04b.\n", b)
 	}
-	b = Toggle(b, 0)
+	b = b.Toggle(0)
 	if b != 0b1101 {
 		t.Fatalf("Expected bit 0 to be set, instead got %04b.\n", b)
 	}
 
-	b = Toggle(b, 3)
+	b = b.Toggle(3)
 	if b != 0b0101 {
 		t.Fatalf("Expected bit 3 to be clear, instead got %04b.\n", b)
 	}
-	b = Toggle(b, 3)
+	b = b.Toggle(3)
 	if b != 0b1101 {
 		t.Fatalf("Expected bit 0 to be set, instead got %04b.\n", b)
 	}
@@ -87,19 +85,21 @@ func TestCorrectlyToggledBit(t *testing.T) {
 func TestCorrectlyChecksdBit(t *testing.T) {
 	b := BitField(0b1101)
 
-	if !Has(b, 0) {
+	if !b.Has(0) {
 		t.Fatalf("Expected check bit 0 to be true.\n")
 	}
-	if Has(b, 1) {
+	if b.Has(1) {
 		t.Fatalf("Expected check bit 1 to be false.\n")
 	}
-	if !Has(b, 2) {
+	if !b.Has(2) {
 		t.Fatalf("Expected check bit 2 to be true.\n")
 	}
-	if !Has(b, 3) {
+	if !b.Has(3) {
 		t.Fatalf("Expected check bit 3 to be true.\n")
 	}
-	if Has(b, BITFIELD_SIZE) {
+	if b.Has(BITFIELD_SIZE) {
 		t.Fatalf("Expected check bit beyond bitfield size to be false.\n")
 	}
+	// var v bitfield
+
 }
