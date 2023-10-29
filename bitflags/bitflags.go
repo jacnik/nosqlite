7489b63ee6c827307daa33ed9a52aa35b8d03fc8
Jacek Kubicki <jacnik@gmail.com>
2023-10-29 16:20:02 +0100

    added tests for bitflags

--- bitflags/bitflags.go
+++ bitflags/bitflags.go
@@ -6,15 +6,26 @@ const BITFIELD_SIZE = 64
 type BitField uint64
 
 // const (
-// 	F0 BitField = 1 << iota
-// 	F1
-// 	F2
+//
+//	F0 BitField = 1 << iota
+//	F1
+//	F2
+//
 // )
+// const BITFIELD_ONE = BitField(1)
 
-func Set(b, flag BitField) BitField    { return b | flag }
-func Clear(b, flag BitField) BitField  { return b &^ flag }
-func Toggle(b, flag BitField) BitField { return b ^ flag }
-func Has(b, flag BitField) bool        { return b&flag != 0 }
+const BITFIELD_EMPTY = BitField(0)
+const BITFIELD_FULL = BitField(0xFFFF_FFFF_FFFF_FFFF)
+
+func Set(b BitField, pos int) BitField    { return 1<<pos | b }
+func Clear(b BitField, pos int) BitField  { return ^(1 << pos) & b }
+func Toggle(b BitField, pos int) BitField { return 1<<pos ^ b }
+func Has(b BitField, pos int) bool        { return 1<<pos&b != 0 }
+
+// func Set(b, flag BitField) BitField    { return b | flag }
+// func Clear(b, flag BitField) BitField  { return b &^ flag }
+// func Toggle(b, flag BitField) BitField { return b ^ flag }
+// func Has(b, flag BitField) bool        { return b&flag != 0 }
 
 // func main() {
 // 	var b BitField
--- bitflags/bitflags_test.go
+++ bitflags/bitflags_test.go
@@ -6,10 +6,100 @@ import (
 
 // Check if bits are Set correctly.
 func TestCorrectlySetsBit(t *testing.T) {
-	b := BitField(1)
-	b = Set(b, 2)
+	b := BITFIELD_EMPTY
 
-	if !Has(b, 4) {
-		t.Fatalf("Expected bit at position 3 to be set, instead got %04b.\n", b)
+	b = Set(b, 2)
+	if b != 0b100 {
+		t.Fatalf("Expected bit 2 to be set, instead got %04b.\n", b)
+	}
+
+	b = Set(b, 0)
+	if b != 0b101 {
+		t.Fatalf("Expected bit 0 to be set, instead got %04b.\n", b)
+	}
+	b = Set(b, 3)
+	if b != 0b1101 {
+		t.Fatalf("Expected bit 4 to be set, instead got %04b.\n", b)
+	}
+	b = Set(b, BITFIELD_SIZE)
+	if b != 0b1101 {
+		t.Fatalf("Expected bits beyond bitfield size nevet to be set.\n")
+	}
+}
+
+// Check if bits are Cleared correctly.
+func TestCorrectlyClearsBit(t *testing.T) {
+	b := BitField(0b1101)
+
+	b = Clear(b, 2)
+	if b != 0b1001 {
+		t.Fatalf("Expected bit 2 to be clear, instead got %04b.\n", b)
+	}
+
+	b = Clear(b, 0)
+	if b != 0b1000 {
+		t.Fatalf("Expected bit 0 to be clear, instead got %04b.\n", b)
+	}
+	b = Clear(b, 3)
+	if b != 0b0000 {
+		t.Fatalf("Expected bit 3 to be clear, instead got %04b.\n", b)
+	}
+	b = Clear(b, 0)
+	if b != 0b0000 {
+		t.Fatalf("Expected bit 0 to be clear, instead got %04b.\n", b)
+	}
+}
+
+// Check if bits are Toggled correctly.
+func TestCorrectlyToggledBit(t *testing.T) {
+	b := BitField(0b1101)
+
+	// v := b.Toggle(2)
+	b = Toggle(b, 2)
+	if b != 0b1001 {
+		t.Fatalf("Expected bit 2 to be clear, instead got %04b.\n", b)
+	}
+	b = Toggle(b, 2)
+	if b != 0b1101 {
+		t.Fatalf("Expected bit 2 to be set, instead got %04b.\n", b)
+	}
+
+	b = Toggle(b, 0)
+	if b != 0b1100 {
+		t.Fatalf("Expected bit 0 to be clear, instead got %04b.\n", b)
+	}
+	b = Toggle(b, 0)
+	if b != 0b1101 {
+		t.Fatalf("Expected bit 0 to be set, instead got %04b.\n", b)
+	}
+
+	b = Toggle(b, 3)
+	if b != 0b0101 {
+		t.Fatalf("Expected bit 3 to be clear, instead got %04b.\n", b)
+	}
+	b = Toggle(b, 3)
+	if b != 0b1101 {
+		t.Fatalf("Expected bit 0 to be set, instead got %04b.\n", b)
+	}
+}
+
+// Check if bits are checked correctly.
+func TestCorrectlyChecksdBit(t *testing.T) {
+	b := BitField(0b1101)
+
+	if !Has(b, 0) {
+		t.Fatalf("Expected check bit 0 to be true.\n")
+	}
+	if Has(b, 1) {
+		t.Fatalf("Expected check bit 1 to be false.\n")
+	}
+	if !Has(b, 2) {
+		t.Fatalf("Expected check bit 2 to be true.\n")
+	}
+	if !Has(b, 3) {
+		t.Fatalf("Expected check bit 3 to be true.\n")
+	}
+	if Has(b, BITFIELD_SIZE) {
+		t.Fatalf("Expected check bit beyond bitfield size to be false.\n")
 	}
 }
